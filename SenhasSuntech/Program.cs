de152600dacfdfd57f3573abb20e4101f43090ea
joao.desouza <joao.desouza@mottu.com.br>
2024-09-20 14:48:19 -0300

    [feat] - melhora do algoritmo + uso de private key

--- SenhasSuntech/Program.cs
+++ SenhasSuntech/Program.cs
@@ -1,13 +1,26 @@
-﻿using System.Security.Cryptography;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Generate4GPassword
 {
     public class GeneratePassword
     {
+        private static IConfiguration _configuration;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Application Started!");
+
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+
+            _configuration = builder.Build();
+
+            var private_key = _configuration["KEYS:PRIVATE_KEY"]?.ToString();
 
             Console.WriteLine("Digite o número do IMEI (Completo): ");
             string imei = Console.ReadLine();
@@ -19,33 +32,43 @@ namespace Generate4GPassword
 
             Console.WriteLine($"Gerando senha para o imei {imei}...");
 
-            string password = GenerateEncriptedPassword(imei);
-
-            Console.WriteLine(password);
-            Console.ReadLine();
+            if (private_key != null)
+            {
+                string password = GenerateEncriptedPassword(imei, private_key);
+                Console.WriteLine($"SENHA PARA O IOT: {password}");
+                Console.ReadLine();
+            }
+            else
+                throw new Exception("Private key é nula");
         }
 
-        public static string GenerateEncriptedPassword(string imei)
+        public static string GenerateEncriptedPassword(string imei, string private_key)
         {
-            SHA256 sha = SHA256.Create();
-            byte[] hashValue;
+            byte[] imeiBytes = Encoding.UTF8.GetBytes(imei);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(private_key);
 
-            UTF8Encoding utf8Encoding = new UTF8Encoding();
-            hashValue = sha.ComputeHash(utf8Encoding.GetBytes(imei));
+            byte[] combinedBytes = new byte[imeiBytes.Length + keyBytes.Length];
+            Buffer.BlockCopy(imeiBytes, 0, combinedBytes, 0, imeiBytes.Length);
+            Buffer.BlockCopy(keyBytes, 0, combinedBytes, imeiBytes.Length, keyBytes.Length);
 
+            int iterations = 10000;
+            int numBytesResponse = 18;
 
-            StringBuilder stringBuilder = new StringBuilder();
-
-            foreach (byte b in hashValue)
+            var pbkdf2 = new Rfc2898DeriveBytes(combinedBytes, combinedBytes, iterations, HashAlgorithmName.SHA256);
+            using (pbkdf2)
             {
-                stringBuilder.Append(b.ToString("X2"));
+                byte[] hashValues = pbkdf2.GetBytes(numBytesResponse);
+
+                StringBuilder hexString = new StringBuilder();
+                foreach(byte b in hashValues)
+                {
+                    hexString.Append(b.ToString("X2"));
+                }
+
+                var password = hexString.ToString().Substring(6, 18);
+
+                return password;
             }
-
-            string hashHex = stringBuilder.ToString();
-
-            var hashNumerico = Math.Abs(Convert.ToInt64(hashHex.Substring(0, 15), 16));
-
-            return hashNumerico.ToString();
         }
     }
 }
